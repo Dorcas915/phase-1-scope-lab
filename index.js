6aeafe98ba85de1405984efedc88689c17d13878
Dorcas Chepkoech <dorcaschepkoech717@gmail.com>
2025-09-20 14:52:12 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Variable in the global scope
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  // Implicitly creates a global variable
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+// Constant variable
+const leastFavoriteCustomer = 'alice';
+
+function changeLeastFavoriteCustomer() {
+  // This will throw an error because leastFavoriteCustomer is a constant
+  leastFavoriteCustomer = 'charlie';
+}
+
